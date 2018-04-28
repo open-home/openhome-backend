d8016f3ae7cd88730c4cda20a20e25e5433abf3d
Simone Di Cicco <simone.dicicco@gmail.com>
2018-04-28 20:36:37 +0200

    Project general refactor

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,12 @@ import { HttpServer } from "./services/http";
 import { DvrService } from "./services/dvr";
 import { LifxLightService } from "./services/lifx";
 import { OpenhomeCloud } from "./services/openhome-cloud";
-import { IConfig } from "./models/interfaces/config.interface";
 import { getConfig, setConfig } from "./utils/utils";
 
 const httpServer = new HttpServer(8080);
 const openhomeCloud = new OpenhomeCloud();
 const lifxLightService = new LifxLightService();
 const dvrService = new DvrService();
-let configState: IConfig;
 
 async function init() {
 
